82f027649cc6d43213acb9264f9972f2a7f50b64
Ebrahem Droobi <edroobi8@gmail.com>
2024-03-06 18:26:42 +0200

    Added connection to mongoDB Now the project runs and connects to the desired db

--- src/main/java/com/management/kumitmanagement/KarateChampionshipApplication.java
+++ src/main/java/com/management/kumitmanagement/KarateChampionshipApplication.java
@@ -4,13 +4,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class KarateChampionshipApplication {
     private static final Logger logger = LoggerFactory.getLogger(KarateChampionshipApplication.class);
 
     public static void main(String[] args) {
         logger.info("Management - Kumite Management - Main - Started the application");
         SpringApplication.run(KarateChampionshipApplication.class, args);
+    }
+
+    @GetMapping
+    public String starterMessage(){
+        return "Welcome to the Karate Championship Management App";
     }
 }
