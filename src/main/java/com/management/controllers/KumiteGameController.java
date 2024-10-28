01cf52e707437f6b2beff22788a700226fb25e5e
Ebrahem Droobi <edroobi8@gmail.com>
2024-10-28 20:06:34 +0200

    Fixed population of players in games

--- src/main/java/com/management/controllers/KumiteGameController.java
+++ src/main/java/com/management/controllers/KumiteGameController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Duration;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/kumiteGames")
@@ -15,18 +14,16 @@ public class KumiteGameController {
     @Autowired
     private KumiteGameService kumiteGameService;
 
-    //TODO posting a game isn't saving the players correctly
     @PostMapping
     public KumiteGame createKumiteGame(@RequestBody KumiteGame gameDetails){
         return kumiteGameService.createKumiteGame(gameDetails.getPlayers(), gameDetails.getReferees(), gameDetails.getDuration());
     }
 
     @GetMapping("/{gameId}")
-    public Optional<KumiteGame> getKumiteGame(@PathVariable String gameId){
+    public KumiteGame getKumiteGame(@PathVariable String gameId){
         return kumiteGameService.getKumiteGame(gameId);
     }
 
-    //TODO Fix the endpoint (not working through postman)
     @PostMapping("/{gameId}/addTime")
     public void addTime(@PathVariable String gameId, @RequestParam int seconds){
         kumiteGameService.addTime(gameId, Duration.ofSeconds(seconds));
