acf52b36616173f513f8c84c0000f02439d20169
Ebrahem Droobi <edroobi8@gmail.com>
2024-10-31 21:39:43 +0200

    Removed kumiteGame field from Referee and updated constructors

--- src/main/java/com/management/models/Referee.java
+++ src/main/java/com/management/models/Referee.java
@@ -2,27 +2,15 @@ package com.management.models;
 
 public class Referee {
     private String name;
-    private KumiteGame kumiteGame;
 
-    public Referee(String name, KumiteGame kumiteGame) {
+    public Referee(String name) {
         this.name = name;
-        this.kumiteGame = kumiteGame;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
-    }
-
-    public KumiteGame getKumiteGame() {
-        return kumiteGame;
-    }
-
-    public void setKumiteGame(KumiteGame kumiteGame) {
-        this.kumiteGame = kumiteGame;
     }
 }
 
