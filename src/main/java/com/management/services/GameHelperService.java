cca561ed32cc10587803eab3d65a7caecfc8dc2b
Guillermo <guillermohe2005@gmail.com>
2026-08-13 12:25:05 +0200

    chore: remove unused Logger debris from GameHelperService

--- src/main/java/com/management/services/GameHelperService.java
+++ src/main/java/com/management/services/GameHelperService.java
@@ -5,15 +5,12 @@ import com.management.enums.PlayerColor;
 import com.management.models.KumiteGame;
 import com.management.models.Player;
 import com.management.util.KumiteGameManagementUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GameHelperService {
 
-    private static final Logger logger = LoggerFactory.getLogger(GameHelperService.class);
 
     @Autowired
     private KumiteGameService kumiteGameService;
